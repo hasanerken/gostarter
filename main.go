e1ce12eac6728b4829baef560a10b69d570241ba
hasanerken <hasan.erken@gmail.com>
2022-01-05 10:43:05 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"golang-starter/internal/logger"
+)
+
+//go:generate go run github.com/google/wire/cmd/wire
+//go:generate go run github.com/swaggo/swag/cmd/swag init
+
+func main() {
+	// you didn't define port in env file
+	// the default port is random from fiber
+	// init log
+	logger.InitLogger()
+
+	InitHttpProtocol().Listen()
+}
